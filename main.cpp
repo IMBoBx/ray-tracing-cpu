2f3760d6afa7ff27852e2945b1fc0e0e875b0f0e
IMBoBx <garvkundnaniyt@gmail.com>
2026-02-11 00:28:42 +0530

    ray class

--- main.cpp
+++ main.cpp
@@ -1,21 +1,53 @@
 #include <iostream>
 
 #include "color.h"
+#include "ray.h"
+#include "vec3.h"
+
+const color ray_color(const ray& r) {
+    vec3 unit_dir = unit_vector(r.direction());
+    auto a = 0.5 * (unit_dir.y() + 1.0);
+    return (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.2, 0.4, 1.0);
+}
 
 int main() {
-    int height = 256;
-    int width = 256;
+    auto aspect_ratio = 16 / 9;
+    int image_width = 400;
 
-    std::cout << "P3\n" << width << " " << height << "\n255\n";
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
 
-    for (int j = 0; j < height; j++) {
-        std::clog << "\rScanlines remaining: " << (height - j) << ' '
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width =
+        viewport_height * (double(image_width) / image_height);
+
+    auto camera_center = point3(0, 0, 0);
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
+                               viewport_u / 2 - viewport_v / 2;
+    auto pixel00_loc =
+        viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
                   << std::flush;
 
-        for (int i = 0; i < width; i++) {
-            color clr =
-                color(double(i) / (width - 1), double(i) / (height - 1), 0.0);
-            write_color(std::cout, clr);
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_center =
+                pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\rDone.                 \n";
