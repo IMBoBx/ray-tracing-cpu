2d5efb07210eac6e2301b9db82511a18514766ed
IMBoBx <garvkundnaniyt@gmail.com>
2026-02-11 01:36:53 +0530

    add sphere and shade using surface normals 6.1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
+#include <cmath>
 #include <iostream>
 
 #include "color.h"
 #include "ray.h"
 #include "vec3.h"
 
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    vec3 dir = r.direction();
+
+    double a = dot(dir, dir);
+    double b = -2.0 * dot(dir, oc);
+    double c = dot(oc, oc) - radius * radius;
+
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+    }
+}
+
 const color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+
     vec3 unit_dir = unit_vector(r.direction());
     auto a = 0.5 * (unit_dir.y() + 1.0);
     return (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.2, 0.4, 1.0);
